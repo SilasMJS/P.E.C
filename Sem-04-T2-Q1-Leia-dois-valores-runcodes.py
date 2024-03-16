5d01b5d656f34a850238ec53eea947d3627db28c
SilasMJS <silasmalaquias71@gmail.com>
2024-03-16 19:20:11 -0300

    funcao

--- Sem-04-T2-Q1-Leia-dois-valores-runcodes.py
+++ Sem-04-T2-Q1-Leia-dois-valores-runcodes.py
@@ -1,37 +1,40 @@
+def transformar(valor,funcao):
+    return funcao(valor)
+
 #A variável vai receber um valor real informado pelo usuário
 a = float(input("Digite um Valor: "))
 #A Variável vai receber um valor inteiro informado pelo usuário
-b = int(input("Digite um Valor: "))
+b = float(input("Digite um Valor: "))
 #Soma dos Numeros 
-print(f"A Soma de {a}+{b} é {a+b}")
+print(f"A Soma de {a}+{b} é {transformar(a,int)+transformar(b,int)}")
 #Concatenação das strings
 #A Variável vai ser convertida ao tipo string
-a = str(a)
+#a = transfomar(a,str)
 #A Variável vai ser convertido ao tipo string
-b = str(b)
+#b = str(b)
 #Exibição do resultado
-print(f"A Concatenação de {a}+{b} é {a+b}")
+print(f"A Concatenação de {a}+{b} é {transformar(a,str)+transformar(b,str)}")
 #Multiplicação dos numeros
 #Avariável vai ser convertida ao tipo float/real
-a = float(a)
+#a = float(a)
 #A Variável vai ser convertida ao tipo float/real
-b = float(b)
+#b = float(b)
 #Vai ser imprimido o resultado
-print(f"A Multiplicação de {a}*{b} é {a*b}")
+print(f"A Multiplicação de {a}*{b} é {transformar(a,float)*transformar(b,float)}")
 #Multiplicação como strings
 #A Variável vai ser convertida ao tipo string
-a = str(a)
+#a = str(a)
 #A Variável vai ser convertida ao tipo inteiro
-b = int(b)
+#b = int(b)
 #Vai ser exibido o resultado
-print(f"A Multiplicação com caractere é {a*b}")
+print(f"A Multiplicação com caractere é {transformar(a,str)*transformar(b,int)}")
 #Divisão dos Números
 #A Variável vai ser convertida ao tipo float/real
-a = float(a)
+#a = float(a)
 # A variável vai ser convertida ao tipo float/real
-b = float(b)
+#b = float(b)
 #O resultado vai ser exibido na tela
-print(f"A divisão de {a}/{b} é {a/b}")
+print(f"A divisão de {a}/{b} é {transformar(a,float)/transformar(b,float)}")
 #Divisão Inteira dos Números
 #O resultado vai ser exibido
 print(f"A Divisão Inteira de {a}//{b} é {a//b}")
@@ -39,7 +42,7 @@ print(f"A Divisão Inteira de {a}//{b} é {a//b}")
 #A Variável vai ser convertida ao tipo int/inteiro
 b = int(b)
 #O resultado vai ser exibido
-print(f"A Exponenciação de {a}**{b} é {a**b}")
+print(f"A Exponenciação de {a}**{b} é {a**transformar(b,int)}")
 #O Módulo (resto)
 #O resultado vai ser exibido
 print(f"O Resto de {a}%{b} é {a%b}")

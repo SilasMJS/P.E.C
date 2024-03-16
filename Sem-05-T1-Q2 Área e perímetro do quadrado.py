bfa3fa9a86bfd0944ceeb10ec34900bbee577cf8
SilasMJS <silasmalaquias71@gmail.com>
2024-03-16 18:25:54 -0300

    second commit

--- Sem-05-T1-Q2 Área e perímetro do quadrado.py
+++ Sem-05-T1-Q2 Área e perímetro do quadrado.py
@@ -1,8 +1,14 @@
-def calcular(l):
+def area(l):
     a = l**2
+    return a
+def perimetro(l):
     p = l*4
-    return a, p
-l = float(input())
+    return p
 
-a , p = calcular(l)
-print(f"{a:10.4f}\n{p:10.4f}")
+def main():
+    l = float(input())
+
+    print(f"{area(l):10.4f}\n{perimetro(l):10.4f}")
+
+if __name__ == "__main__":
+    main()

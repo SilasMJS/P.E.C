ef38090dbfc70545fc5c8fe1d3b2d8545aa235b1
SilasMJS <silasmalaquias71@gmail.com>
2024-03-23 16:20:51 -0300

    23/03/2024 16:20

--- Sem-06-T1-Q3-Tempo-de-duracao.py
+++ Sem-06-T1-Q3-Tempo-de-duracao.py
@@ -1,13 +1,22 @@
+# função auxiliar onde sera executado uma so tarefa
 def hora(seg):
+    # retorno da função retornando o resultado
     return seg // 3600
+# função auxiliar onde sera executado uma so tarefa
 def minuto(seg):
+    # retorno da função retornando o resultado
     return (seg%3600) // 60
+# função auxiliar onde sera executado uma so tarefa
 def segundo(seg):
+    # retorno da função retornando o resultado
     return (seg%3600)%60
-
+# função principal onde esta armazenado o codigo principal
 def main():
-    seg = int(input())
-    print(f"{hora(seg)}:{minuto(seg)}:{segundo(seg)}")
-
+    # entrada de dado onde tambem vai ser armazenado na variável o dado informado pelo usuário
+    seg = int(input("Digite Quantos Segundos se Passaram do Evento: "))
+    # vai ser exibido o resultado na tela do usuário
+    print(f"Ja se passaram {hora(seg)}:{minuto(seg)}:{segundo(seg)} do evento")
+# condição que verifica se a função/modulo é a principal se for vai ser chamada a função e executada
 if __name__ == "__main__":
+    # chamando a função main
     main()
--- Sem-06-T1-Q4-Minimo-maximo-media.py
+++ Sem-06-T1-Q4-Minimo-maximo-media.py
@@ -15,24 +15,38 @@
 
 # if __name__ == "__main__":
 #     main()
-
+# função auxiliar onde sera exectudado de preferencia uma tarefa especifica
 def maior(valor):
+    # retorno da função retornando o resultado obtido da função
     return max(valor)
+# função auxiliar onde sera execudado de preferencia uma tarefa especifica
 def menor(valor):
+    # retorno da função retornando o resultado obtido da função 
     return min(valor)
+# função auxiliar onde sera execudado de preferencia uma tarefa especifica
 def media(valor):
+    # retorno da função retornando o resultado obtido da função
     return sum(valor) / len(valor)
-
+# função principal onde fica o codigo principal
 def main():
-    n1 = int(input())
-    n2 = int(input())
-    n3 = int(input())
-    n4 = int(input())
-    n5 = int(input())
-
+    # entrada de dados onde tambem sera armazenado na variável o dado informado pelo usuário
+    n1 = int(input("Digite o Primeiro Número: "))
+    # entrada de dados onde tambem sera armazenado na variável o dado informado pelo usuário
+    n2 = int(input("Digite o Segundo Número: "))
+    # entrada de dados onde tambem sera armazenado na variável o dado informado pelo usuário
+    n3 = int(input("Digite o Terceiro Número: "))
+    # entrada de dados onde tambem sera armazenado na variável o dado informado pelo usuário
+    n4 = int(input("Digite o Quarto Número: "))
+    # entrada de dados onde tambem sera armazenado na variável o dado informado pelo usuário
+    n5 = int(input("Digite o Quinto Número: "))
+    # armazenando as variaveis e uma so no formato de vetor
     valor = [n1,n2,n3,n4,n5]
-
-    print(f"{maior(valor)}\n{menor(valor)}\n{media(valor)}")
-
+    # exibindo o resultado na telo do usuário
+    print(f"""Números Digitados: {n1}, {n2}, {n3}, {n4}, {n5}
+    O Maior é {maior(valor)}
+    O Menor é {menor(valor)}
+    A Média é {media(valor)}""")
+# condição que verificar se o modulo/função é o principal se for a função é chamada e executada
 if __name__ == "__main__":
+    # chamando a função main
     main()

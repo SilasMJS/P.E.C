7deba55d66c8906337cc413f53fc64532bafc8b2
SilasMJS <silasmalaquias71@gmail.com>
2024-03-30 10:18:01 -0300

    30/03/24 10:17

--- Sem-07-T1-Q5-calculo-media-ajuste.py
+++ Sem-07-T1-Q5-calculo-media-ajuste.py
@@ -1,22 +1,32 @@
+# função auxiliar onde sera executado de preferencia uma tarefa especifica
 def media(n1, n2, n3):
+    # condição se for verdadeira vai ser executado o bloco contido nela
     if n3 > 8:
+        # condição se for verdadeira vai ser executado o bloco contido nela
         if ((n1+n2+n3)/3)+1 > 10:
+            # variavel recebendo um valor inteiro
             media = 10
+            # retorno, retornando o resultado
             return media
+        # se não entrar na condição acima ira executar o bloco contido nela
         else:
+            # retorno, retonando o resultado
             return ((n1+n2+n3)/3)+1
-            
-      
+    # se não entrar na condição acima ira executar o bloco contido nela
     else:
+        # retorno, retornando o resultado
         return (n1+n2+n3)/3
-
+# função principal onde fica contido o codigo principal
 def main():
-    n1 = float(input())
-    n2 = float(input())
-    n3 = float(input())  
-
-    print(f"{media(n1, n2, n3):.2f}")
-
-
+    # variável recebendo e armazenando o dado informado pelo usuário
+    n1 = float(input("Digite a Primeira Nota: "))
+    # variável recebendo e armazenando o dado informado pelo usuário
+    n2 = float(input("Digite a Segunda Nota: "))
+    # variável recebendo e armazenando o dado informado pelo usuário  
+    n3 = float(input("Digite a Terceira Nota: "))
+    # exibindo o resultado na tela do usuário
+    print(f"Sua Média Final é {media(n1, n2, n3):.2f}")
+# condição que verificar se a função/modulo é o principal se for vai chamar e executar a função main
 if __name__ == "__main__":
+    # chamando a função main
     main()
--- Sem-07-T2-Q5-3-numeros-ordenados.py
+++ Sem-07-T2-Q5-3-numeros-ordenados.py
@@ -1,20 +1,34 @@
-def maximo(n,n2,n3):
+# função auxiliar onde sera executado de preferencia uma tarefa especifica
+def crescente(n,n2,n3):
+    # condição se for verdadeira vai executar o bloco contido nela
     if n >= n2:
+        # condição se for verdadeira vai executar o bloco contido nela
         if n2 >= n3:
-            return (f"{n3}\n{n2}\n{n}")
-        return (f"{n2}\n{n3}\n{n}")
+            # retornando o resultado
+            return (f"{n3}, {n2}, {n}")
+        # retornando o resultado
+        return (f"{n2}, {n3}, {n}")
+    # se a condição de cima for falsa verifica se essa é verdadeira se for vai executar o bloco contido nela
     elif n2 >= n3:
+        # condição se for verdadeira vai executar o bloco contido nela
         if n3 >= n:
-            return (f"{n}\n{n3}\n{n2}") 
-        return (f"{n3}\n{n}\n{n2}")
-    return (f"{n}\n{n2}\n{n3}")
-    
-
+            # retornando o resultado
+            return (f"{n}, {n3}, {n2}")
+        # retornando o resultado 
+        return (f"{n3}, {n}, {n2}")
+    # retornando o resultado
+    return (f"{n}, {n2}, {n3}")   
+# função principal onde fica o codigo principal
 def main():
-    n = int(input())
-    n2 = int(input())
-    n3 = int(input())
-    print(f"{maximo(n,n2,n3)}")
-    
+    # a variável vai receber e armazenar o dado informado pelo usuário
+    n = int(input("Digite o Primeiro Número: "))
+    # a variável vai receber e armazenar o dado informado pelo usuário
+    n2 = int(input("Digite o Segundo Número: "))
+    # a variável vai receber e armazenar o dado informado pelo usuário
+    n3 = int(input("Digite o Terceiro Número: "))
+    # vai ser exibido na tela do usuário o resultado
+    print(f"A ordem Crescente dos Números {n,n2,n3} é {crescente(n,n2,n3)}")
+# condição que verificar se a função/modulo é o principal se for vai chamar e executar a função main   
 if __name__ == "__main__":
+    # chamando a função main
     main()

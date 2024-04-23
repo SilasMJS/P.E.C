02aab0f3c379577f39fae2ff7f09f3aa1cf7bdae
SilasMJS <silasmalaquias71@gmail.com>
2024-04-23 17:33:09 -0300

    17:32 23/04/2024

--- Sem-10-T2-SilasMalaquias/sem-T2-Q5-simular-compra-parcelada.py
+++ Sem-10-T2-SilasMalaquias/sem-T2-Q5-simular-compra-parcelada.py
@@ -1,18 +1,15 @@
-# def parcelamento(valor):
-#     for i in range(1,25):
-#         parc = valor/i
-#         if i < 25:
-#             print(f"{i}x de R$ {parc:.2f}")
-#         else:
-#             print("Limite de parcelas atingidas!!")
-        
-def main():
-    valor = float(input())
-    # parcelas = parcelamento(valor)
-    # print(parcelas)
+def parcelamento(valor):
     for i in range(1,25):
         parc = valor/i
         print(f"{i}x de R$ {parc:.2f}")
+        
+def main():
+    valor = float(input())
+    parcelamento(valor)
+ 
+    # for i in range(1,25):
+    #     parc = valor/i
+    #     print(f"{i}x de R$ {parc:.2f}")
     
 if __name__ == "__main__":
     main()

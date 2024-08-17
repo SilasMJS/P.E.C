fe2dcdaf0f42e59225becc9856b1116fd6df6062
SilasMJS <silasmalaquias71@gmail.com>
2024-08-17 18:39:04 -0300

    18:38 17/08/2024

--- Sem-11-T1-SilasMalaquias/sem-11-T1-Q5-dividanocartao.py
+++ Sem-11-T1-SilasMalaquias/sem-11-T1-Q5-dividanocartao.py
@@ -26,3 +26,37 @@ def main():
 # condição que verificar se a função/modulo é o principal se for vai chamar e executar
 if __name__ == "__main__":
     main()
+
+
+# def dever(valor_divida):
+#     valor_divida = valor_divida * (0.15)
+#     return valor_divida
+# def aumento(valor_salario):
+#     valor_salario = valor_salario * (0.05)
+#     return valor_salario
+
+# def divida_salario(salario,divida):
+#     ano = 2016
+#     mes = 10
+#     salario_final = salario
+#     divida_final = divida
+    
+#     while True:
+#         mes += 1
+#         if mes > 12:
+#             mes = 1
+#             ano += 1
+#         if mes == 3:
+#             salario_final += aumento(salario_final)
+#         divida_final += dever(divida_final)
+#         if divida_final >= salario_final:
+#             return f"{mes}/{ano}"
+           
+# def main():
+#     salario = float(input())
+#     divida = float(input())
+    
+#     print(divida_salario(salario,divida))
+    
+# if __name__ == "__main__":
+#     main()
